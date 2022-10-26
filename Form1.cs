ea3cb29c4c4574d63813ca6e1e842a55a78234be
Dmitrii Palienko <dmitrii.palienko@gmail.com>
2022-10-26 19:15:28 +0300

    New Logic Implementation

--- Form1.cs
+++ Form1.cs
@@ -1,37 +1,82 @@
 using System.Data;
 using System.Text.RegularExpressions;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections;
 using System.Windows.Forms;
 using org.mariuszgromada.math.mxparser;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
-
+using System.Drawing;
 namespace Excel_Parody
 {
+
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
         }
-        DataTable table = new DataTable();
-        Dictionary<string, string[]> Cell_Dictionary = new Dictionary<string, string[]>();
-        private void Form1_Load(object sender, EventArgs e)
+        private static DataTable table = new DataTable();
+        private static SortedDictionary<string, string[]> Cell_Dictionary = new SortedDictionary<string, string[]>(new SortByRows());
+        public static void convert_From_Dict_To_Table()
         {
-            for (int i = 1; i < 7; i++)
+            table.Rows.Clear();
+            table.Columns.Clear();
+            var index_of_R_C = Cell_Dictionary.Keys.Last();
+            var parsing_Row_Col = Regex.Matches(index_of_R_C, @"\d+");
+            var rows_c = Int32.Parse(parsing_Row_Col[0].ToString());
+            var col_c = Int32.Parse(parsing_Row_Col[1].ToString());
+
+            for (int i_c = 1; i_c <= col_c; i_c++)
             {
-                //dataGridView1.Columns.Add(Convert.ToString(i), Convert.ToString(i));
                 table.Columns.Add((table.Columns.Count + 1).ToString());
             }
-            for (int i = 1; i < 7; i++)
+            for (int i_r = 1; i_r <= rows_c; i_r++)
             {
                 table.Rows.Add();
             }
-            for (int i = 1; i < 7; i++)
+            convert_From_Expression_To_Value();
+
+        }
+        public static void convert_From_Expression_To_Value()
+        {
+            var index_of_R_C = Cell_Dictionary.Keys.Last();
+            var parsing_Row_Col = Regex.Matches(index_of_R_C, @"\d+");
+            var rows_c = Int32.Parse(parsing_Row_Col[0].ToString());
+            var col_c = Int32.Parse(parsing_Row_Col[1].ToString());
+            dynamic expression_RegEX;
+            dynamic mx_type_Expression;
+            string expression;
+            string pattern = @"[R]\d[C]\d";
+
+            for (int i_r = 1; i_r <= rows_c; i_r++)
             {
-                for(int a = 1; a < 7; a++)
+                for (int i_c = 1; i_c <= col_c; i_c++)
                 {
-                    Cell_Dictionary.Add("R" + i.ToString() + "C" + a.ToString(), new []{"",""});
+                    if (Cell_Dictionary["R" + (i_r).ToString() + "C" + (i_c).ToString()][1] != "")
+                    {
+                        expression = Cell_Dictionary["R" + (i_r).ToString() + "C" + (i_c).ToString()][1];
+                        expression_RegEX = Regex.Replace(expression, pattern,
+                    m => Cell_Dictionary[m.Value][0]);
+                        mx_type_Expression = new Expression(expression_RegEX);
+                        Cell_Dictionary["R" + (i_r).ToString() + "C" + (i_c).ToString()][0] = mx_type_Expression.calculate().ToString();
+                        table.Rows[i_r - 1][i_c - 1] = Cell_Dictionary["R" + (i_r).ToString() + "C" + (i_c).ToString()][0]; ;
+                    }
+                    else
+                        table.Rows[i_r - 1][i_c - 1] = Cell_Dictionary["R" + (i_r).ToString() + "C" + (i_c).ToString()][0];
                 }
             }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 1; i <= 7; i++)
+            {
+                for (int a = 1; a <= 7; a++)
+                {
+                    Cell_Dictionary.Add("R" + i.ToString() + "C" + a.ToString(), new[] { "", "" });
+                }
+            }
+            convert_From_Dict_To_Table();
 
             dataGridView1.DataSource = table;
         }
@@ -43,6 +88,7 @@ namespace Excel_Parody
         // Add Row
         private void button1_Click(object sender, EventArgs e)
         {
+            /*
             table.Rows.Add();
             int count_of_rows = table.Rows.Count;
             int count_of_columns = table.Columns.Count;
@@ -50,11 +96,23 @@ namespace Excel_Parody
             {
                 Cell_Dictionary.Add("R" + (count_of_rows).ToString() + "C" +  a.ToString(), new[] {"",""});
             }
+            */
+
+            var index_of_R_C = Cell_Dictionary.Keys.Last();
+            var parsing_Row_Col = Regex.Matches(index_of_R_C, @"\d+");
+            var rows_c = Int32.Parse(parsing_Row_Col[0].ToString());
+            var col_c = Int32.Parse(parsing_Row_Col[1].ToString());
+
+            for (int i = 1; i <= col_c; i++)
+            {
+                Cell_Dictionary.Add("R" + (rows_c + 1).ToString() + "C" + i.ToString(), new[] { "", "" });
+            }
+            convert_From_Dict_To_Table();
         }
         // Add Column
         private void button2_Click(object sender, EventArgs e)
         {
-            //dataGridView1.Columns.Add(Convert.ToString(dataGridView1.Columns.Count + 1), Convert.ToString(dataGridView1.Columns.Count + 1));
+            /*
             table.Columns.Add((table.Columns.Count + 1).ToString());
             int count_of_rows = table.Rows.Count;
             int count_of_columns = table.Columns.Count;
@@ -62,6 +120,17 @@ namespace Excel_Parody
             {
                 Cell_Dictionary.Add("R" + (a).ToString() + "C" + (count_of_columns).ToString(), new[] {"",""});
             }
+            */
+            var index_of_R_C = Cell_Dictionary.Keys.Last();
+            var parsing_Row_Col = Regex.Matches(index_of_R_C, @"\d+");
+            var rows_c = Int32.Parse(parsing_Row_Col[0].ToString());
+            var col_c = Int32.Parse(parsing_Row_Col[1].ToString());
+
+            for (int i = 1; i <= rows_c; i++)
+            {
+                Cell_Dictionary.Add("R" + (i).ToString() + "C" + (col_c + 1).ToString(), new[] { "", "" });
+            }
+            convert_From_Dict_To_Table();
         }
         // Adding Indexes to Rows
         private void dataGridView1_RowPostPaint(object sender, DataGridViewRowPostPaintEventArgs e)
@@ -82,6 +151,7 @@ namespace Excel_Parody
         private void button2_Click_1(object sender, EventArgs e)
         {
             int column_ind = dataGridView1.CurrentCell.ColumnIndex;
+            /*
             int count_of_rows = table.Rows.Count;
             int count_of_columns = table.Columns.Count;
             table.Columns.RemoveAt(column_ind);
@@ -112,12 +182,34 @@ namespace Excel_Parody
             {
 
             }
+            */
+            var index_of_R_C = Cell_Dictionary.Keys.Last();
+            var parsing_Row_Col = Regex.Matches(index_of_R_C, @"\d+");
+            var rows_c = Int32.Parse(parsing_Row_Col[0].ToString());
+            var col_c = Int32.Parse(parsing_Row_Col[1].ToString());
+            for (int a = 1; a <= rows_c; a++)
+            {
+                for (int i = column_ind + 1; i < col_c; i++)
+                {
+                    Cell_Dictionary["R" + (a.ToString()) + "C" + (i.ToString())][0] = Cell_Dictionary["R" + (a.ToString()) + "C" + (i + 1).ToString()][0];
+                    Cell_Dictionary["R" + (a.ToString()) + "C" + (i.ToString())][1] = Cell_Dictionary["R" + (a.ToString()) + "C" + (i + 1).ToString()][1];
+                }
+
+            }
+            for (int i = 1; i <= rows_c; i++)
+            {
+                Cell_Dictionary.Remove("R" + (i).ToString() + "C" + (col_c).ToString());
+            }
+            convert_From_Dict_To_Table();
+
+
         }
 
         // Delete Row
         private void button1_Click_1(object sender, EventArgs e)
         {
             int rows_ind = dataGridView1.CurrentCell.RowIndex;
+            /*
             int count_of_rows = table.Rows.Count;
             int count_of_columns = table.Columns.Count;
             table.Rows.RemoveAt(rows_ind);
@@ -134,7 +226,25 @@ namespace Excel_Parody
             {
                 Cell_Dictionary.Remove("R" + (count_of_rows).ToString() + "C" + (i.ToString()));
             }
+            */
+            var index_of_R_C = Cell_Dictionary.Keys.Last();
+            var parsing_Row_Col = Regex.Matches(index_of_R_C, @"\d+");
+            var rows_c = Int32.Parse(parsing_Row_Col[0].ToString());
+            var col_c = Int32.Parse(parsing_Row_Col[1].ToString());
+            for (int a = 1; a <= col_c; a++)
+            {
+                for (int i = rows_ind + 1; i < rows_c; i++)
+                {
+                    Cell_Dictionary["R" + (i.ToString()) + "C" + (a.ToString())][0] = Cell_Dictionary["R" + (i + 1).ToString() + "C" + (a.ToString())][0];
+                    Cell_Dictionary["R" + (i.ToString()) + "C" + (a.ToString())][1] = Cell_Dictionary["R" + (i + 1).ToString() + "C" + (a.ToString())][1];
+                }
 
+            }
+            for (int i = 1; i <= col_c; i++)
+            {
+                Cell_Dictionary.Remove("R" + (rows_c).ToString() + "C" + (i.ToString()));
+            }
+            convert_From_Dict_To_Table();
 
         }
         // Show Cell
@@ -150,6 +260,7 @@ namespace Excel_Parody
 
         private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
         {
+            /*
             int rows_index = e.RowIndex;
             int columns_index = e.ColumnIndex;
             if (rows_index >= 0 && columns_index >= 0)
@@ -165,12 +276,34 @@ namespace Excel_Parody
                     textBox2.Text = "R" + (rows_index + 1).ToString() + "C" + (columns_index + 1).ToString();
                 }
             }
+            */
+            int rows_ind = dataGridView1.CurrentCell.RowIndex;
+            int cols_ind = dataGridView1.CurrentCell.ColumnIndex;
+            textBox1.Text = Cell_Dictionary["R" + (rows_ind + 1).ToString() + "C" + (cols_ind + 1).ToString()][1];
+            textBox2.Text = "R" + (rows_ind + 1).ToString() + "C" + (cols_ind + 1).ToString();
         }
 
         private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
         {
-            
+           
+
             string expression = dataGridView1.CurrentCell.Value.ToString();
+            string pattern = @"[R]\d[C]\d";
+            dynamic expression_RegEX;
+            dynamic mx_type_Expression;
+
+            int rows_ind = dataGridView1.CurrentCell.RowIndex;
+            int cols_ind = dataGridView1.CurrentCell.ColumnIndex;
+
+            expression_RegEX = Regex.Replace(expression, pattern,
+                    m => Cell_Dictionary[m.Value][0]);
+            mx_type_Expression = new Expression(expression_RegEX);
+
+            Cell_Dictionary["R" + (rows_ind + 1).ToString() + "C" + (cols_ind + 1).ToString()][0] = mx_type_Expression.calculate().ToString();
+            Cell_Dictionary["R" + (rows_ind + 1).ToString() + "C" + (cols_ind + 1).ToString()][1] = expression;
+
+            convert_From_Dict_To_Table();
+            /*
             var pattern = @"[R]\d[C]\d";
             var stringVariableMatches = Regex.Replace(expression, pattern,
                 m => Cell_Dictionary[m.Value][1]);
@@ -182,6 +315,7 @@ namespace Excel_Parody
 
             string text_expression = express_ion1.getExpressionString().ToString();
             Cell_Dictionary["R" + (e.RowIndex + 1).ToString() + "C" + (e.ColumnIndex + 1).ToString()][0] = text_expression;
+            */
         }
     }
 }
---
+++ SortByRows.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Excel_Parody
+{
+    class SortByRows : IComparer<string>
+    {
+        public int Compare(string x, string y)
+        {
+            var first_str = Regex.Matches(x, @"\d+");
+            var second_str = Regex.Matches(y, @"\d+");
+            var f_num = Convert.ToInt32(first_str[0].ToString() + first_str[1].ToString());
+            var s_num = Convert.ToInt32(second_str[0].ToString() + second_str[1].ToString());
+            if (f_num == s_num)
+            {
+                return 0;
+            }
+            if (f_num < s_num)
+            {
+                return -1;
+            }
+            else
+            {
+                return 1;
+            }
+
+        }
+    }
+
+}
